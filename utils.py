cdf6d7f20650aafa56dd13d92a83ad13e1f0da35
Hsiu W Wang <hsiu.w.wang@cnsdc01.pwc.com>
2018-06-17 14:13:57 +0800

    another implementation of greedy.

--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
 import random
 
 def greedy(qfunc, state, actions):
-    amax = 0
-    key = "%d_%s" % (state, actions[0])
-    qmax = qfunc[key]
-    for i in range(len(actions)):  # 扫描动作空间得到最大动作值函数
-        key = "%d_%s" % (state, actions[i])
-        q = qfunc[key]
-        if qmax < q:
-            qmax, amax = q, i
-    return actions[amax]
+    # amax = 0
+    # key = "%d_%s" % (state, actions[0])
+    # qmax = qfunc[key]
+    # for i in range(len(actions)):  # 扫描动作空间得到最大动作值函数
+    #     key = "%d_%s" % (state, actions[i])
+    #     q = qfunc[key]
+    #     if qmax < q:
+    #         qmax, amax = q, i
+    # return actions[amax]
+    state_rewards = { key: value for key, value in qfunc.items() if int(key.split("_")[0]) == state }
+    max_reward = max(zip(state_rewards.values(), state_rewards.keys()))
+    return max_reward[1].split("_")[1]
 
 def epsilon_greedy(qfunc, state, actions, epsilon):
     amax = 0
